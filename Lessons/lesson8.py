ed6db398d9446763a043c7cc95bc0abe208c3afb
Isaiah-Martinez <82429414+Isaiah-Martinez@users.noreply.github.com>
2023-04-21 12:55:52 -0700

    lesson8 completed on UNIX machine

--- Lessons/lesson8.py
+++ Lessons/lesson8.py
@@ -1,35 +1,23 @@
-import subprocess, shlex
+import subprocess, shlex, os
 
-command = "dir week3"
-print(command)
+command = "ls -l /Users/ez_roma/Desktop/DnD"
 
-proc = subprocess.Popen(shlex.split(command), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,)
+proc = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT,)
 
-files = []
+fileNumb = 1
+size = -1
+dirname = ''
 for line in iter(proc.stdout.readline, b''):
-    parts = line.decode('utf-8').strip()
-    print(parts)
-
-'''
-#print(proc.stdout.readline())
-
-
-for line in iter(proc.stdout.readline()):
-    print(line)
-
-print('\n\n\n')
-
-print(proc.stdout)
-print(type(proc.stdout))
-print(proc.stdout.readable())
-print(proc.stdout.read())
-#, shell=True
-
-command = 'ls -l some/folder'
-
-process = subprocess.Popen(shlex.split(command), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-
-for line in iter(process.stdout.readline, ''):
-
-
-'''
+    #skip the initial number of files in the directory
+    if fileNumb == 1:
+        fileNumb = 0
+    else:
+        #find the max file size
+        locSpace = line.decode('utf-8').strip().split()
+        newPath = shlex.split(command)[2] + '/' + locSpace[len(locSpace) - 1]
+        if os.path.isdir(newPath):            
+            if size < int(locSpace[4]):
+                size = int(locSpace[4])
+                dirname = locSpace[len(locSpace) - 1]
+            
+print('Directory name:', dirname, 'has size', size, 'MB')
